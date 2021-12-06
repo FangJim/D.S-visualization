d01f345a306b3fd81d39f2eb83568db991d3de76
Jim Fang <jmjmjim1101@gmail.com>
2021-12-06 19:05:02 +0800

    Starting array.pug

--- public/js/array.js
+++ public/js/array.js
@@ -1,3 +1,4 @@
+//event listen
 const insertValue = document.querySelector('.insert');
 const DeleteValue = document.querySelector('.delete');
 
@@ -7,3 +8,22 @@ go.forEach((go) => {
 
     })
 })
+
+
+//canvas
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+ctx.lineWidth = 5;
+ctx.strokeStyle = 'white';
+ctx.font = '20px Arial';
+ctx.fillStyle = 'white';
+for (let i = 0; i < 20; i++) {
+    //first line
+    ctx.strokeRect(0 + i * 100, 50, 100, 100)
+    ctx.fillText(`${i}`, 45 + i * 100, 45)
+    //second line
+    ctx.strokeRect(0 + i * 100, 250, 100, 100)
+    ctx.fillText(`${i + 10}`, 40 + i * 100, 245)
+}
+
+
