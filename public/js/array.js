6af6b1bdca1ce3d17f8ece963036d59d40eb9302
Jim Fang <jmjmjim1101@gmail.com>
2021-12-07 20:14:59 +0800

    Finish Array

--- public/js/array.js
+++ public/js/array.js
@@ -1,43 +1,69 @@
-//canvas array format
-const array = {
-    lineWidth: 5,
-    strokeStyle: 'white',
-    font: '20px Arial',
-    fillStyle: 'white',
-    width: 100,
-    height: 100
-}
-
-//canvas circle format
-const circle = {
-    size: 20,
-    strokeStyle: 'red',
-}
-
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-let arr = new Array(20).fill(null);
+let arr = new Array(20).fill("");
+
+//for knowing insert or delete now
+let flag = {
+    state: false,
+    goto: null
+};
+
 update();
+
+//main
 function update() {
-    init();
+
+    drawArray();
+
+    if (flag.state) {
+        drawCircle(flag.goto)
+    } else {
+        drawSlash(flag.goto)
+    }
 
     requestAnimationFrame(update)
 }
-//draw array
-function init() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.lineWidth = array.lineWidth;
-    ctx.strokeStyle = array.strokeStyle;
-    ctx.font = array.font;
-    ctx.fillStyle = array.fillStyle;
-    for (let i = 0; i < 20; i++) {
-        //first line
-        ctx.strokeRect(0 + i * 100, 50, array.width, array.height)
-        ctx.fillText(`${i}`, 45 + i * 100, 45)
 
-        //second line
-        ctx.strokeRect(0 + i * 100, 250, array.width, array.height)
-        ctx.fillText(`${i + 10}`, 40 + i * 100, 245)
+//draw array
+function drawArray() {
+    let offset;
+
+    //array setting
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = 'white';
+    ctx.font = '25px Arial';
+    ctx.fillStyle = 'white';
+
+    for (let i = 0; i < 20; i++) {
+        //adjust font offset
+        if (arr[i] > 999) {
+            offset = 25;
+        }
+        else if (arr[i] > 99) {
+            offset = 30;
+        }
+        else if (arr[i] > 9) {
+            offset = 35;
+        }
+        else {
+            offset = 45;
+        }
+
+
+        //draw array
+        if (i < 10) {
+            //first line
+            ctx.strokeRect(0 + i * 100, 50, 100, 100)
+            ctx.fillText(`${i}`, 45 + i * 100, 45)
+            ctx.fillText(`${arr[i]}`, offset + i * 100, 110)
+        }
+        else {
+            //second line
+            ctx.strokeRect(0 + (i - 10) * 100, 250, 100, 100)
+            ctx.fillText(`${i}`, 35 + (i - 10) * 100, 245)
+            ctx.fillText(`${arr[i]}`, offset + (i - 10) * 100, 310)
+        }
     }
 }
 
@@ -52,10 +78,24 @@ function drawCircle(index) {
     circle_x += (index % 10) * 100;
 
     ctx.beginPath();
-    ctx.arc(circle_x, circle_y, circle.size, 0, Math.PI * 2);
-    ctx.strokeStyle = circle.strokeStyle;
+    ctx.arc(circle_x, circle_y, 50, 0, Math.PI * 2);
+    ctx.strokeStyle = 'red';
     ctx.stroke();
+}
 
+function drawSlash(index) {
+    if (flag.goto != null) {
+        let line_x = 0;
+        let line_y = 50;
+        if (index > 9) {
+            line_y = 250;
+        }
+        ctx.beginPath();
+        ctx.moveTo(line_x + (index % 10) * 100, line_y);
+        ctx.lineTo(line_x + (index % 10) * 100 + 100, line_y + 100);
+        ctx.strokeStyle = 'red';
+        ctx.stroke();
+    }
 }
 
 //event listen
@@ -67,10 +107,45 @@ const insertGo = document.querySelector('.insertGo');
 const deleteGo = document.querySelector('.deleteGo');
 
 insertGo.addEventListener('click', () => {
-    drawCircle(insertIndex.value);
+    //both need to have value
+    if (insertIndex.value == "" || insertValue.value == "") {
+        return;
+    }
+
+    //out of range
+    if (insertIndex.value > 19) {
+        insertIndex.value = ""
+        alert('Index out of range')
+        return;
+    }
+
+    //insert to array
     arr[insertIndex.value] = insertValue.value;
+
+    //effect variable
+    flag.state = true
+    flag.goto = insertIndex.value
+
+    //init
+    insertIndex.value = ""
+    insertValue.value = ""
 });
 
 deleteGo.addEventListener('click', () => {
-    arr[deleteIndex.value] = null;
+    //out of range
+    if (deleteIndex.value > 19) {
+        deleteIndex.value = ""
+        alert('Index out of range')
+        return;
+    }
+
+    //delete to array
+    arr[deleteIndex.value] = "";
+
+    //effect variable
+    flag.state = false
+    flag.goto = deleteIndex.value
+
+    //init
+    deleteIndex.value = "";
 });
