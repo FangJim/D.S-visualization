cf1411a7b39511d8eb92eed92553fd6e692dc30e
Jim Fang <jmjmjim1101@gmail.com>
2021-12-09 12:59:21 +0800

    Stack views

--- public/js/stack.js
+++ public/js/stack.js
@@ -14,36 +14,61 @@ function Stack() {
         items = [];
     }
     this.print = function () {
-        console.log(items.toString());
+        console.log(items);
     }
 }
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-
+//build stack
+let stack = new Stack();
 
 update();
 
+//main
 function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawStack();
+    currentObj();
 
     requestAnimationFrame(update)
 }
 
-function drawStack() {
+function currentObj() {
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = 'white';
 
+    ctx.strokeRect(100, 50, 300, 50);
 }
 
-const pushValue = document.querySelector('.pushValue');
+function drawStack() {
+    //setting
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(350, 100);
+    ctx.lineTo(350, 480);
+    ctx.moveTo(350, 480);
+    ctx.lineTo(650, 480);
+    ctx.moveTo(650, 480);
+    ctx.lineTo(650, 100);
+    ctx.strokeStyle = 'white';
+    ctx.stroke();
+}
 
+//event listen
+const pushValue = document.querySelector('.pushValue');
 const pushGo = document.querySelector('.pushGo');
 const popGo = document.querySelector('.popGo');
 
 
 pushGo.addEventListener('click', function () {
-
+    if (pushValue.value == "") alert('Please insert a numbers');
+    stack.push(pushValue.value)
+    stack.print();
+    pushValue.value = "";
 })
 popGo.addEventListener('click', function () {
-
+    stack.pop();
+    stack.print();
 })
 
